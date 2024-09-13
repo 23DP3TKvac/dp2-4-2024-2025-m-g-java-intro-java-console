772add93c6542477ea77ef84586a9e5f3f401b50
23DP3TKvac <166509350+23DP3TKvac@users.noreply.github.com>
2024-09-13 11:26:31 +0000

    Uzdevums 0

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,9 +1,23 @@
 package lv.rvt;
 
+import java.util.*;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        // Make your Hello World programm!
+        Scanner scanner = new Scanner(System.in);
+
+
+        String name = scanner.nextLine();
+
+        String lastname = scanner.nextLine();
+
+        String group = scanner.nextLine();
+
+        System.out.println("Students: " + name + " " + lastname + " " + "No grupas: " + group);
+
+
+
     }
 }
