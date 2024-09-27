ee6bd9015a15db0cf3f38b5a54472035eaf4e160
23DP3TKvac <166509350+23DP3TKvac@users.noreply.github.com>
2024-09-27 09:39:23 +0000

    Speeding ticket

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -4,30 +4,15 @@ import java.util.*;
 
 public class App 
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Give the first number:");
-        int first = Integer.valueOf(scanner.nextLine());
-        System.out.println("Give me the second number:");
-        int second = Integer.valueOf(scanner.nextLine());
+        System.out.println("Give speed:");
 
-        int result1 = first + second;
-        System.out.println("8" + " " + "+" + " " + "2" + " " + " " + "=" + " " + result1);
-        int result2 = first - second;
-        System.out.println("8" + " " + "-" + " " + "2" + " " + " " + "=" + " " + result2);
-        int result3 = first * second;
-        System.out.println("8" + " " + "*" + " " + "2" + " " + " " + "=" + " " + result3);
-        double result4 = first / second;
-        System.out.println("8" + " " + "/" + " " + "2" + " " + " " + "=" + " " + result4);
-
-
-        
-
-
-
-
+        int number = Integer.valueOf(scanner.nextLine());
+        if (number > 120) {
+    System.out.println("Speeding ticket!");
+}
 
     }
 }
