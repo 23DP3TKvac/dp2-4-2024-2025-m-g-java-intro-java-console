59d2deec24a9b8b0c32361ab72dde5e3ff7200cb
23DP3TKvac <166509350+23DP3TKvac@users.noreply.github.com>
2024-10-10 11:30:11 +0000

    Number and sum of numbers

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -5,23 +5,29 @@ import java.util.*;
 public class App 
 {
     public static void main( String[] args ) {
+
         Scanner scanner = new Scanner(System.in);
+        int NumberOfNumbers = 0;
+        int sum = 0;
 
-        System.out.println("Value of the gift?");
-        int gift = Integer.valueOf(scanner.nextLine());
+        while (true){
+            System.out.println("Give a number:");
+            int Usernumber = Integer.valueOf(scanner.nextLine());
 
-        if (gift < 5000) {
-            System.out.println("No tax!");
-        } else if (gift >= 5000 && gift <=24999) {
-            System.out.println("Tax:" + " " + (double)(100+(gift - 5000) * 0.08));
-        } else if (gift >= 25000 && gift <=54999) {
-            System.out.println("Tax:" + " " + (double)(1700+(gift - 25000) * 0.1));
-        } else if (gift >= 55000 && gift <=199999) {
-            System.out.println("Tax:" + " " + (double)(4700+(gift - 55000) * 0.12));
-        }else if (gift >= 200000 && gift <=999999) {
-            System.out.println("Tax:" + " " + (double)(22100+(gift - 200000) * 0.15));
-        }else if (gift >= 1000000) {
-            System.out.println("Tax:" + " " + (double)(142100+(gift - 1000000) * 0.17));
+            if (Usernumber == 0) {
+                break;
+            }
+
+            if (Usernumber > 0 || Usernumber < 0) {
+                NumberOfNumbers = NumberOfNumbers + 1;
+                sum = sum + Usernumber;
+            }
+        }    
+        if (NumberOfNumbers != 0){
+            System.out.println("Number of numbers:" + " " + NumberOfNumbers);
+            System.out.println("Sum of the numbers:" + " " + sum);
         }
-}
+    
+
+}      
 }
