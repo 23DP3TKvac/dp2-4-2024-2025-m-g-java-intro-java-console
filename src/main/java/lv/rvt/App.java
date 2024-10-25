c2f238d30123549792958bc121ec342067ca994f
23DP3TKvac <166509350+23DP3TKvac@users.noreply.github.com>
2024-10-25 10:26:17 +0000

    Advanced astrology (3 parts)

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -4,15 +4,26 @@ package lv.rvt;
 public class App 
 {
     public static void main( String[] args ) {
-        printRectangle(17, 3);
+        printTriangle(4);
     }
-    public static void printRectangle(int width, int height) {
-        for (int i = 0; i < width; i++){
-            for (int a = 0; a < height; a++){
-                System.out.print("*");
-            }
-            
-        }
-    } 
-}      
+    
 
+    public static void printSpaces(int size) {
+        for(int j = 0; j < size; j++){
+            System.out.print(" ");
+        }
+    }
+    public static void printTriangle(int size) {
+        for (int i = 0; i < size; i++){                                             
+            printSpaces(size - i - 1);
+            printStars(i + 1);
+            System.out.println("");
+        }
+    }
+
+    public static void printStars(int number) {
+        for(int a = 0; a < number; a++){
+            System.out.print("*");
+        }
+    }
+}
